bebdebfdaa16d568ee938b62773bdef45169d3e1
Cool&Ouf <cooletouf1@gmail.com>
2025-04-13 21:02:54 +0200

    Sync albums test

--- syncDigiToNextcloud.py
+++ syncDigiToNextcloud.py
@@ -1,20 +1,26 @@
+import os
 import time
+import os
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from checkDigikamFaceTags import check_person_unicity_for_digikam, check_person_unicity_for_each_image
 from syncFaces import sync_faces, create_all_facerecog_persons, create_all_facerecog_images
+from syncTagsAsAlbums import create_all_oc_photos_albums
 
-NEXTCLOUD_USER = 'NEXTCLOUD_USER'
-NEXTCLOUD_ROOTPATH = 'NEXTCLOUD_ROOT_PATH'
-NEXTCLOUD_RECOGNIZE_MODEL = 1
-NEXTCLOUD_STORAGE = 9
+from dotenv import load_dotenv  # Pour la gestion de l'environnement DEVELOPPEMENT/PROD
+env_path = load_dotenv()
+
+NEXTCLOUD_USER = os.environ.get('NEXTCLOUD_USER')
+NEXTCLOUD_ROOTPATH = os.environ.get('NEXTCLOUD_ROOTPATH')
+NEXTCLOUD_RECOGNIZE_MODEL = os.environ.get('NEXTCLOUD_RECOGNIZE_MODEL')
+NEXTCLOUD_STORAGE = os.environ.get('NEXTCLOUD_STORAGE')
 
 debut = time.time()
 
 # Connexion à Nextcloud (MySQL)
-NEXTCLOUD_DATABASE_URL = "mysql+pymysql://DATABASE_CREDENTIALS"
+NEXTCLOUD_DATABASE_URL = os.environ.get('NEXTCLOUD_DATABASE_URL')
 nc_engine = create_engine(NEXTCLOUD_DATABASE_URL, echo=True)
 
 # Création d'une session Nextcloud
@@ -22,7 +28,7 @@ NC_Session = sessionmaker(bind=nc_engine)
 nc_session = NC_Session()
 
 # Connexion à DIGIKAM (sqlite)
-DIGIKAM_DATABASE_URL = "sqlite:////Users/DIGIKAM_SQLITE"
+DIGIKAM_DATABASE_URL = os.environ.get('DIGIKAM_DATABASE_URL')
 dgk_engine = create_engine(DIGIKAM_DATABASE_URL, echo=True)
 
 # Création d'une session Digikam
@@ -33,12 +39,18 @@ has_person_unicity_problem = check_person_unicity_for_digikam(dgk_session)
 has_image_person_unicity_problem = check_person_unicity_for_each_image(dgk_session)
 
 if has_person_unicity_problem is False and has_image_person_unicity_problem is False:
+
+    create_all_oc_photos_albums(NEXTCLOUD_USER, NEXTCLOUD_ROOTPATH, NEXTCLOUD_STORAGE, nc_session, dgk_session)
+
     create_all_facerecog_persons(NEXTCLOUD_USER, nc_session, dgk_session)
     create_all_facerecog_images(NEXTCLOUD_USER, NEXTCLOUD_ROOTPATH, NEXTCLOUD_RECOGNIZE_MODEL, NEXTCLOUD_STORAGE,
                                 nc_session, dgk_session)
     sync_faces(NEXTCLOUD_USER, NEXTCLOUD_ROOTPATH, NEXTCLOUD_RECOGNIZE_MODEL, nc_session, dgk_session)
 else:
     print("Synchronisation annulée à cause de problèmes d'unicité")
+
+
+
 
 nc_session.close()
 dgk_session.close()
